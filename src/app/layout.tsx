ce4d01a94addc746327d31723345ffda1c9ffbc0
Stormy Adams <stormy251251@gmail.com>
2023-10-10 15:02:17 -0700

    feat(analytics): add analytics to this project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react';
 import type { Metadata } from 'next';
 
 import CommandMenu from '@/features/app/components/CommandMenu';
@@ -41,6 +42,7 @@ export default function RootLayout({
             <Toaster />
           </AppContextProvider>
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   );
